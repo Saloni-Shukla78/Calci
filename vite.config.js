a7c575a6c253451e5b3993aaf9b1b77cd89d7a18
Siddharth Tiwari <tiwarisid022018@gmail.com>
2025-02-03 19:34:13 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/calculator/',
+  base: '/Calci/',  // Ensure this matches your repository name
   plugins: [react()],
 })
